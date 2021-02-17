ab4406fcc1101239260f83cc392543795eb4be2f
kj312621833 <kj312621833@gmail.com>
2021-02-17 22:45:00 +0800

    Add files via upload

---
+++ 1487A - Arena.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int main() {
+  int t;
+  cin >> t;
+  while (t--) {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int& x : a) cin >> x;
+    cout << n - count(a.begin(), a.end(), *min_element(a.begin(), a.end())) << endl;
+  }
+}
